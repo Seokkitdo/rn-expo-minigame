2de773f49b810f7570d509fa126523c24953f30e
Seokkitdo <sjd12329@gmail.com>
2023-12-04 10:47:23 +0900

    기능 : 난수 생성

---
+++ screens/GameScreen.tsx
@@ -0,0 +1,45 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+import Title from '../components/ui/Title';
+import { useState } from 'react';
+import NumberContainer from '../components/game/NumberContainer';
+
+function generateRandomBetween(min: number, max: number, exclude: number) {
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+}
+
+interface GameScreenProps {
+  userChoice: number;
+}
+
+function GameScreen({ userChoice }: GameScreenProps) {
+  const initialGuess = generateRandomBetween(1, 100, userChoice);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  return (
+    <View style={styles.screen}>
+      <Title>Opponent's Guess</Title>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <View>
+        <Text>Higher of lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
+}
+
+export default GameScreen;
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
